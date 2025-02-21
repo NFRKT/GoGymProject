80be6731d1a3824f947358719136676d8ea987ad
NFRKT <janekdziwok@o2.pl>
2025-02-21 20:37:29 +0100

    Commit -  Main page

--- src/main/java/com/GoGym/controller/UserController.java
+++ src/main/java/com/GoGym/controller/UserController.java
@@ -1,4 +1,5 @@
 package com.GoGym.controller;
+import com.GoGym.repository.UserRepository;
 import com.GoGym.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,9 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import com.GoGym.dto.UserRegistrationDTO;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @AllArgsConstructor
@@ -17,6 +16,9 @@ public class UserController {
     @Autowired
     @Lazy
     private final UserService userService;
+
+    @Autowired
+    private final UserRepository userRepository;
 
     @GetMapping("/login")
     public String Login(Model model) {
@@ -31,10 +33,19 @@ public class UserController {
         return "registerUser";
     }
     @PostMapping("/register")
-    public String processRegister(@ModelAttribute UserRegistrationDTO userDTO) {
-        userService.registerUser(userDTO);
-        return "redirect:/login";
+    public String processRegister(@ModelAttribute UserRegistrationDTO userDTO, Model model) {
+        try {
+            userService.registerUser(userDTO);
+            return "redirect:/login";
+        } catch (IllegalStateException e) {
+            // Dodajemy komunikat błędu do modelu
+            model.addAttribute("emailErrorMessage", e.getMessage());
+            // Ustawiamy ponownie formularz (jeśli chcesz, aby użytkownik nie musiał wpisywać wszystkiego od nowa)
+            model.addAttribute("User", userDTO);
+            return "registerUser";
+        }
     }
+
 }
 
 
